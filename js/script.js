2b063d627aeb5f5f1e5b6bb5c6a6a27c96f12ef2
WalterCorsini <corsini.walter@gmail.com>
2024-04-27 18:14:59 +0200

    add complete boolzap without comment

--- js/script.js
+++ js/script.js
@@ -59,123 +59,123 @@ createApp({
                 }
                 ],
                 },
-                // {
-                // name: 'Samuele',
-                // avatar: '_3',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '28/03/2020 10:10:40',
-                // message: 'La Marianna va in campagna',
-                // status: 'received'
-                // },
-                // {
-                // date: '28/03/2020 10:20:10',
-                // message: 'Sicuro di non aver sbagliato chat?',
-                // status: 'sent'
-                // },
-                // {
-                // date: '28/03/2020 16:15:22',
-                // message: 'Ah scusa!',
-                // status: 'received'
-                // }
-                // ],
-                // },
-                // {
-                // name: 'Alessandro B.',
-                // avatar: '_4',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '10/01/2020 15:30:55',
-                // message: 'Lo sai che ha aperto una nuova pizzeria?',
-                // status: 'sent'
-                // },
-                // {
-                // date: '10/01/2020 15:50:00',
-                // message: 'Si, ma preferirei andare al cinema',
-                // status: 'received'
-                // }
-                // ],
-                // },
-                // {
-                // name: 'Alessandro L.',
-                // avatar: '_5',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '10/01/2020 15:30:55',
-                // message: 'Ricordati di chiamare la nonna',
-                // status: 'sent'
-                // },
-                // {
-                // date: '10/01/2020 15:50:00',
-                // message: 'Va bene, stasera la sento',
-                // status: 'received'
-                // }
-                // ],
-                // },
-                // {
-                // name: 'Claudia',
-                // avatar: '_6',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '10/01/2020 15:30:55',
-                // message: 'Ciao Claudia, hai novità?',
-                // status: 'sent'
-                // },
-                // {
-                // date: '10/01/2020 15:50:00',
-                // message: 'Non ancora',
-                // status: 'received'
-                // },
-                // {
-                // date: '10/01/2020 15:51:00',
-                // message: 'Nessuna nuova, buona nuova',
-                // status: 'sent'
-                // }
-                // ],
-                // },
-                // {
-                // name: 'Federico',
-                // avatar: '_7',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '10/01/2020 15:30:55',
-                // message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                // status: 'sent'
-                // },
-                // {
-                // date: '10/01/2020 15:50:00',
-                // message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                // status: 'received'
-                // }
-                // ],
-                // },
-                // {
-                // name: 'Davide',
-                // avatar: '_8',
-                // visible: true,
-                // messages: [
-                // {
-                // date: '10/01/2020 15:30:55',
-                // message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                // status: 'received'
-                // },
-                // {
-                // date: '10/01/2020 15:50:00',
-                // message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                // status: 'sent'
-                // },
-                // {
-                // date: '10/01/2020 15:51:00',
-                // message: 'OK!!',
-                // status: 'received'
-                // }
-                // ],
-                // }
+                {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                {
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
+                },
+                {
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+                },
+                {
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Alessandro B.',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Alessandro L.',
+                avatar: '_5',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ricordati di chiamare la nonna',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Va bene, stasera la sento',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Claudia',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao Claudia, hai novità?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Non ancora',
+                status: 'received'
+                },
+                {
+                date: '10/01/2020 15:51:00',
+                message: 'Nessuna nuova, buona nuova',
+                status: 'sent'
+                }
+                ],
+                },
+                {
+                name: 'Federico',
+                avatar: '_7',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Davide',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:51:00',
+                message: 'OK!!',
+                status: 'received'
+                }
+                ],
+                }
             ],
             rndNumberText: 0,
             arrayText:[
@@ -190,26 +190,27 @@ createApp({
     },
     methods:{
         sendMessage: function(i){
-            const now = luxon.DateTime.now();
-            const dateNow = now.toLocaleString(luxon.DateTime.DATETIME_SHORT_WITH_SECONDS);
-            rndNumberText = Math.floor(Math.random() * this.arrayText.length);
-              if(this.inputMessage !== ""){
-                  const message = {
-                      date: dateNow,
-                      message: this.inputMessage,
-                      info: false,
-                      status: "sent",
-                  };
-                      const messageYou = {
-                      date: dateNow,
-                      message: this.arrayText[rndNumberText],
-                      info: false,
-                      status: "received",
-                  };
+          const now = luxon.DateTime.now();
+          const dateNow = now.toLocaleString(luxon.DateTime.DATETIME_SHORT_WITH_SECONDS);
+          rndNumberText = Math.floor(Math.random() * this.arrayText.length);
+          if(this.inputMessage !== ""){
+            const message = {
+              date: dateNow,
+              message: this.inputMessage,
+              info: false,
+              status: "sent",
+            };
+            const messageYou = {
+              date: dateNow,
+              message: this.arrayText[rndNumberText],
+              info: false,
+              status: "received",
+            };
+            console.log("ciao");
                   this.inputMessage="";
-                  this.contacts[i].messages.push(this.message);
+                  this.contacts[i].messages.push(message);
                   this.onlineMessage = "online sta scrivendo...";
-                  setTimeout(() =>{ this.contacts[i].messages.push(this.messageYou)
+                  setTimeout(() =>{ this.contacts[i].messages.push(messageYou)
                   this.onlineMessage = "online";
                     setTimeout(() =>{
                   this.onlineMessage = "";
@@ -241,28 +242,30 @@ createApp({
               });
           },
           deleteAllMessage: function(){
-            this.contacts[this.count].messages.splice(0,this.contacts[this.count].messages.length);
-            this.menuHam = false;
+            this.contacts[this.count].messages.splice(0);
+            this.menuHamburger = false;
             this.noChat = false;
             },
 
         deleteChat: function(){
             if(this.contacts.length > 1 && this.count !== this.contacts.length-1){
             this.contacts.splice(this.count,1);
-            this.menuHam = false;
+            this.noChat = false;
             } else if(this.contacts.length > 1 && this.contacts.length-1 === this.count ) {
+                this.noChat = false;
                 this.contacts.splice(this.count,1);
                 this.count--;
             } else{
-                this.contacts = [
-                    {
-                    name: "",
-                    visible: false,  
-                    messages: [],
-                    },
-                ];
-                this.noChat = true;
+              this.noChat = true;
+              this.contacts = [
+                {
+                  name: "",
+                  visible: false,  
+                  messages: [],
+                },
+              ];
             }
+            this.menuHamburger = false;
             },
           createChat: function(){
             copy = this.nameNewContact;
@@ -273,7 +276,13 @@ createApp({
                 name: copy,
                 avatar: this.arrayImg[this.rndNumberImg],
                 visible:true,
-                messages: [],
+                messages: [
+                  {
+                    date: "",
+                    message: "",
+                    status: ""
+                  }
+                ],
               }),
               this.noChat = false;
               if(this.contacts[0].name === ""){
